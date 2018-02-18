8bf50497ae00159a4d454e6dd63d143ad6d49120
deft1991 <deft1991@gmail.com>
2018-02-18 14:31:32 +0300

    spring web socket

---
+++ src/main/java/testHelloWorld/GreetingController.java
@@ -0,0 +1,17 @@
+package testHelloWorld;
+
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GreetingController {
+
+    @MessageMapping("/chat")
+    @SendTo("topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + message.getName() + "!");
+    }
+}
